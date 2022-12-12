86f7a4a59d45032cb1e567039618b40ecef6d030
Luanderson almeida <92821001+LuandersonASantos@users.noreply.github.com>
2022-12-12 18:28:16 -0300

    implement login page

--- pom/models/LoginPage.js
+++ pom/models/LoginPage.js
@@ -4,6 +4,19 @@ class BasePage extends BasePage{
             super(page)
 
             this.usernameTxt = '#username'
+            this.passwordTxt = '#password'
+            this.loginBtn = '#log-in'
             
     }
+
+    async navigate(){
+        await super.navigate('index.html');
+    }
+
+    async login(username, password){
+        await this.page.fill(this.usernameTxt, username);
+        await this.page.fill(this.passwordTxt, password);
+        await this.page.click(this.loginBtn);
+    }
 }
+module.exports = LoginPage;
